5d7ae37c5ba5c8d8a1e65cf209262b165480b246
Kaito Udagawa <umireon@gmail.com>
2021-10-18 04:51:54 +0000

    verapdf: add 1.19.206 bottle.

--- Formula/verapdf.rb
+++ Formula/verapdf.rb
@@ -5,6 +5,12 @@ class Verapdf < Formula
   sha256 "879996826c8bae0c9fd2c6f54226c7cdb581d2363869592c44aeec960620e815"
   license any_of: ["GPL-3.0-or-later", "MPL-2.0"]
 
+  bottle do
+    root_url "https://github.com/umireon/homebrew-verapdf/releases/download/verapdf-1.19.206"
+    sha256 cellar: :any_skip_relocation, catalina:     "c314ca8a7146361f0f92c3cb77e6540525f50afe1cb973791f318b17359bd5f0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1be6da77fafd41a891441b62e11ec2f7654745d155489ff1e9c13ed665967d25"
+  end
+
   depends_on "maven" => :build
   depends_on "openjdk@11"
 
