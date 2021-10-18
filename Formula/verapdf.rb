2af5197824cb01ba598adadb299a174091f47fb0
Kaito Udagawa <umireon@gmail.com>
2021-10-18 13:36:09 +0900

    verapdf 1.19.206

--- Formula/verapdf.rb
+++ Formula/verapdf.rb
@@ -1,8 +1,8 @@
 class Verapdf < Formula
   desc "Industry supported, open source PDF/A validation GUI and CUI"
   homepage "https://software.verapdf.org/"
-  url "https://github.com/veraPDF/veraPDF-apps/archive/refs/tags/v1.19.198.tar.gz"
-  sha256 "6a29db1e015db51f31eac46da47ce10ac8b953971b142d440ed89323eede9f7d"
+  url "https://github.com/veraPDF/veraPDF-apps/archive/refs/tags/v1.19.206.tar.gz"
+  sha256 "879996826c8bae0c9fd2c6f54226c7cdb581d2363869592c44aeec960620e815"
   license any_of: ["GPL-3.0-or-later", "MPL-2.0"]
 
   depends_on "maven" => :build
